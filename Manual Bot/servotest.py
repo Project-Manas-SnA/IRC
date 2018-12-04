a70983dfcdb0882e25972686b6dddac212717d45
Rishab Agarwal <rishabgrwl121@gmai.com>
2018-12-04 14:35:43 +0530

    Raspi commit

--- Manual Bot/servotest.py
+++ Manual Bot/servotest.py
@@ -6,18 +6,16 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(12, GPIO.OUT)
 
-p = GPIO.PWM(12, 50)
+p = GPIO.PWM(12, 1000)
 
 p.start(7.5)
 
 try:
-    while True:
-        p.ChangeDutyCycle(7.5)  # turn towards 90 degree
-        time.sleep(1) # sleep 1 second
-        p.ChangeDutyCycle(2.5)  # turn towards 0 degree
-        time.sleep(1) # sleep 1 second
-        p.ChangeDutyCycle(12.5) # turn towards 180 degree
-        time.sleep(1) # sleep 1 second 
+    for i in range(0,200,1):
+	j = i/2 +0.5
+        p.ChangeDutyCycle(j) # turn towards 90 degree
+        time.sleep(1)
+	print(j)
 except KeyboardInterrupt:
     p.stop()
     GPIO.cleanup()
