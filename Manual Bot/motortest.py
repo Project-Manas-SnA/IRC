d42650fb66cff087d86151d22b2d4fd27f22c7f8
RishanAgarwal <rishabgrwl121@gmail.com>
2018-12-07 22:13:12 +0530

    semi-final

--- Manual Bot/motortest.py
+++ Manual Bot/motortest.py
@@ -7,62 +7,57 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-gamepad = InputDevice('/dev/input/event0')
+directionLeft = 17              #physical 11
+directionRight = 27             #physical 13
+GPIO.setup(directionLeft,GPIO.OUT)
+GPIO.setup(directionRight,GPIO.OUT)
 
 GPIO.setup(15,GPIO.OUT)
 GPIO.setup(18,GPIO.OUT)
 
 pwmLeft = GPIO.PWM(15, 1000)    #physical 10
-directionLeft = 17              #physical 11
 pwmRight = GPIO.PWM(18, 1000)   #physical 12
-directionRight = 27             #physical 13
 
-GPIO.setup(directionLeft,GPIO.OUT)
-
-GPIO.setup(directionRight,GPIO.OUT)
-
+gamepad = InputDevice('/dev/input/event0')
 speed = 50
-for event in gamepad.read_loop():
 
-    if event.code == 17:
+    for event in gamepad.read_loop():
 
-        if event.value == -1:
-            GPIO.output(directionLeft,GPIO.LOW)
-            pwmLeft.start(speed)
-            GPIO.output(directionRight,GPIO.HIGH)              #Forward
-            pwmRight.start(speed)
-            print("Forward")
+        if event.code == 17:
 
-	elif event.value == 1:
-            GPIO.output(directionLeft,GPIO.HIGH)
-            pwmLeft.start(speed)                                   #Backward
-            GPIO.output(directionRight,GPIO.LOW)
-            pwmRight.start(speed)
-	    print ("Backwards")
+            if event.value == -1:
+                GPIO.output(directionLeft,GPIO.LOW)
+                pwmLeft.start(speed)
+                GPIO.output(directionRight,GPIO.HIGH)              #Forward
+                pwmRight.start(speed)
+        
+            elif event.value == 1:
+                GPIO.output(directionLeft,GPIO.HIGH)
+                pwmLeft.start(speed)                                   #Backward
+                GPIO.output(directionRight,GPIO.LOW)
+                pwmRight.start(speed)
+            elif event.value == 0:
+                GPIO.output(directionLeft,GPIO.LOW)
+                pwmLeft.stop()                                      #Stop
+                GPIO.output(directionRight,GPIO.LOW)
+                pwmRight.stop()
 
-        elif event.value == 0:
-            GPIO.output(directionLeft,GPIO.LOW)
-            pwmLeft.stop()                                      #Stop
-            GPIO.output(directionRight,GPIO.LOW)
-            pwmRight.stop()
+        elif event.code == 16:
 
-    elif event.code == 16:
+            if event.value == -1:
+                GPIO.output(directionLeft,GPIO.HIGH)
+                pwmLeft.start(speed)
+                GPIO.output(directionRight,GPIO.HIGH)              #Left
+                pwmRight.start(speed)
 
-        if event.value == -1:
-            GPIO.output(directionLeft,GPIO.HIGH)
-            pwmLeft.start(speed)
-            GPIO.output(directionRight,GPIO.HIGH)              #Left
-            pwmRight.start(speed)
-	    print("left")
+            elif event.value == 1:
+                GPIO.output(directionLeft,GPIO.LOW)
+                pwmLeft.start(speed)
+                GPIO.output(directionRight,GPIO.LOW)              #Right
+                pwmRight.start(speed)
 
-        elif event.value == 1:
-            GPIO.output(directionLeft,GPIO.LOW)
-            pwmLeft.start(speed)
-            GPIO.output(directionRight,GPIO.LOW)              #Right
-            pwmRight.start(speed)
-	    print("right")
-        elif event.value == 0:
-            GPIO.output(directionLeft,GPIO.LOW)
-            pwmLeft.stop()                                     #stop
-            GPIO.output(directionRight,GPIO.LOW)
-            pwmRight.stop()
+            elif event.value == 0:
+                GPIO.output(directionLeft,GPIO.LOW)
+                pwmLeft.stop()                                     #stop
+                GPIO.output(directionRight,GPIO.LOW)
+                pwmRight.stop()
