4afb2ebff1ccc5e4eb7b5822b9f0105a2a9389ad
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2018-12-14 12:51:03 +0530

    Add QR code detection in mask.py

--- irc-develop/mask.py
+++ irc-develop/mask.py
@@ -1,4 +1,5 @@
 from box import detectColour
+from qr import decode
 import cv2
 
 cap = cv2.VideoCapture(0)
@@ -7,4 +8,7 @@ if __name__ == "__main__":
     while True:
         ret,frame = cap.read()
         detectColour(frame)
+        data = decode(frame)
+        if data!=None:
+                print(data)
         cv2.waitKey(1)
